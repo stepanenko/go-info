d6b7fefef968e9e061b4c780e86c02b1bc21794c
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-03-12 20:24:10 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,10 @@
+// package declaration:
+package main // makes this file an executable; requires main() function
+
+// importing other packages that we need, standard or reusable from other devs:
+import "fmt" // accessing standard Go library - format
+
+// declare functions, tell Go to do things:
+func main() {
+	fmt.Println("Hi there!")
+}
