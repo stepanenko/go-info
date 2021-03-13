f6e2d0f307bb6568f5adbea5ba0773f580468fc0
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-03-13 13:03:36 +0200

    Add files via upload

---
+++ first/first.go
@@ -0,0 +1,11 @@
+// package declaration:
+package main // makes this file an executable; requires main() function
+
+// importing other packages that we need, standard or reusable from other devs:
+import "fmt" // accessing standard Go library - format
+
+// declare functions, tell Go to do things:
+// main function will be automatically called whenever we execute the 'first' file
+func main() {
+	fmt.Println("Hi there!")
+}
