b17fe71a50999496d0297e55f0c7d44964b73b40
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-03-24 20:57:41 +0200

    Update main.go

--- cards/main.go
+++ cards/main.go
@@ -9,4 +9,7 @@ func main() {
 
 	hand.print()
 	remainingCards.print()
+	
+	fmt.Println([]byte("Hello")) // type conversion, will print [72 101 108 108 111] <- byte slice
+	fmt.Println([]string(hand))  // [Ace of Spades Two of Spades Three of Spades Four of Spades Five of Spades]
 }
